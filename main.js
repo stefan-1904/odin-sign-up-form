9796f663f57b4c90e0875457f4e12d7cf5f75f71
Stefan <sminns1230@gmail.com>
2024-01-31 21:54:58 +0000

    Finish password check logic

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 function passwordMatch() {
-    let pass1 = document.getElementById('password').value;
-    let pass2 = document.getElementById('confirmPassword').value;
+    let pass1 = document.getElementById('password');
+    let pass2 = document.getElementById('confirmPassword');
+    let btn = document.getElementById('createBtn');
+    let checkAlert = document.getElementById('passwordValidation');
 
-    if (pass1 === pass2) {
-        console.log('match')
-    } else if (pass1 != pass2) {
-        console.log('no match')
+    if (pass1 === '' || pass2 === '') {
+        checkAlert.style.display = 'none';
+        console.log('a field is empty')
+    }
+
+    if (pass1.value === pass2.value) {
+        checkAlert.style.display = 'none';
+        pass1.style.outlineColor = '#E5E7EB';
+        pass2.style.outlineColor = '#E5E7EB';
+        btn.disabled = false;
+    } else if (pass1.value != pass2.value) {
+        checkAlert.style.display = 'block';
+        pass1.style.outlineColor = '#B91C1C';
+        pass2.style.outlineColor = '#B91C1C';
+        btn.disabled =  true;
     }
 }
